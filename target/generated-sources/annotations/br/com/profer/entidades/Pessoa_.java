b3a360e4ed2019f3d2f3680ce4bac8bfa3f17544
paulo <pauloons@gmail.com>
2020-11-10 17:11:21 -0300

    Commit Alterações

--- target/generated-sources/annotations/br/com/profer/entidades/Pessoa_.java
+++ target/generated-sources/annotations/br/com/profer/entidades/Pessoa_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-10-30T00:34:08")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-11-10T17:08:45")
 @StaticMetamodel(Pessoa.class)
 public class Pessoa_ { 
 
